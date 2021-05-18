6b9c80b14da77e2bed8acbf0ce9d8807255d0b00
AyrolS <bsartaguda@gmail.com>
2021-05-18 14:59:37 +0800

    Development Branch 2nd Commit

--- src/com/ayrol/Account.java
+++ src/com/ayrol/Account.java
@@ -1,5 +1,23 @@
 package com.ayrol;
 
-public abstract class Account {
+public abstract class Account
+{
+    public int accountNumber;
+    public double balance;
 
+    public Account(int acctNum)
+    {
+        accountNumber = acctNum;
+        setBalance(100.0);
+    }
+
+    public void setBalance(double b)
+    {
+        balance = b;
+    }
+
+    public abstract int getAccountNumber();
+    public abstract double getBalance();
+
+    public abstract String getAccountInfo();
 }
